ace429997f26bf8fd24eb2c599debb23bb03a60c
maheshdhulipudi45 <24p31f00a8@acet.ac.in>
2026-02-08 14:33:09 +0530

    Add root Server.js

--- backend/server.js
+++ backend/server.js
@@ -22,3 +22,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("CampusFix Backend API is running 🚀");
+});
